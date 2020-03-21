49654427de18b0e44fde063c9cc41d9b2f123175
Peter Ma` <shadowfax0230@gmail.com>
2020-03-21 17:21:35 -0400

    update classification

---
+++ DeepSearch/classification.py
@@ -0,0 +1,34 @@
+import numpy as np
+import keras
+from keras.models import Sequential 
+from keras.layers import Dense, Dropout, LSTM, CuDNNLSTM, ConvLSTM2D
+from keras.layers.core import Activation, Flatten
+import matplotlib.pyplot as plt
+from keras.optimizers import SGD,RMSprop,adam
+from keras.models import load_model
+from keras.utils import np_utils
+import os, os.path
+from scipy.io import wavfile
+from keras.models import Model
+from keras import backend as K
+from random import random
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+
+class predict(object):
+
+    def __init__(self, anchor, test, model_location):
+        self.anchor = anchor
+        self.test = test
+        self.encoder_injected = load_model(model_location)
+
+    def compute_distance(self):
+        values= np.zeros(self.test.shape[0])
+        check = self.encoder_injected.predict(self.test)
+        anchor = self.encoder_injected.predict(self.anchor)
+        for j in range(0, self.test.shape[0]-1):
+            index = int(random()*10)
+            values[j]=(np.square(np.subtract(anchor[index:index+1,:], check[j:j+1,:]))).mean()
+        return values
+
