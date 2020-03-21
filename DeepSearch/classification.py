1132ae9f473abbbf20a0954b13fa73c16ea7ed63
Peter Ma` <shadowfax0230@gmail.com>
2020-03-21 17:27:54 -0400

    prediction_update

--- DeepSearch/classification.py
+++ DeepSearch/classification.py
@@ -22,13 +22,20 @@ class predict(object):
         self.anchor = anchor
         self.test = test
         self.encoder_injected = load_model(model_location)
-
+        self.values= np.zeros(self.test.shape[0])
     def compute_distance(self):
-        values= np.zeros(self.test.shape[0])
         check = self.encoder_injected.predict(self.test)
         anchor = self.encoder_injected.predict(self.anchor)
         for j in range(0, self.test.shape[0]-1):
             index = int(random()*10)
-            values[j]=(np.square(np.subtract(anchor[index:index+1,:], check[j:j+1,:]))).mean()
-        return values
-
+            self.values[j]=(np.square(np.subtract(anchor[index:index+1,:], check[j:j+1,:]))).mean()
+        return self.values
+    
+    def max_index(self, top=3):
+        top_hits = []
+        copy = self.values
+        for i in range(0, top):
+            hit = np.argmax(copy)
+            top_hits.append(hit)
+            copy[hit]=0
+        return top_hits
