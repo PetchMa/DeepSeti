123810adc7389fe02ff84d1edaad88bc24ac0bad
Peter Ma` <shadowfax0230@gmail.com>
2020-04-08 11:18:52 -0400

    prediction method fixed bug

--- DeepSeti_utils/predict.py
+++ DeepSeti_utils/predict.py
@@ -24,11 +24,15 @@ class predict(object):
         self.values= np.zeros(self.test.shape[0])
 
     def compute_distance(self):
+        """
+        Method helps compute the MSE between two N-d vectors and is used to make the
+        Helps facilitate fast computation.                 
+        """
         check = self.encoder_injected.predict(self.test)
         anchor = self.encoder_injected.predict(self.anchor)
         for j in range(0, self.test.shape[0]-1):
             # index = int(random()*10)
-            index = 5
+            index = 0
             self.values[j]=(np.square(np.subtract(anchor[index:index+1,:], check[j:j+1,:]))).mean()   
         return self.values
     
@@ -39,14 +43,17 @@ class predict(object):
             hit = np.argmax(copy)
             top_hits.append(hit)
             copy[hit]=0
-
         return top_hits
+
     def min_index(self, top=3):
+        """
+        This method finds the minimum distance - used for reverse image search for signals.
+        This implements the same logic as the max_index search.
+        """
         top_hits = []
         copy = self.values
         for i in range(0, top):
             hit = np.argmin(copy)
             top_hits.append(hit)
             copy[hit]=0
-
         return top_hits
